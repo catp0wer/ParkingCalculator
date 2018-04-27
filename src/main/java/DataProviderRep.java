8dcf47176cef0e2e451a25357d6fd68c3018b935
laila <laila@fake.com>
2018-04-27 23:42:20 +0300

    add a few scenarios

--- src/main/java/DataProviderRep.java
+++ src/main/java/DataProviderRep.java
@@ -8,9 +8,10 @@ import org.testng.annotations.DataProvider;
         public static Object[][] LogInCaseData(){
             return new Object[][]
                     {
-                            { "Economy Parking","4/11/2018"},
 
-
+                            { "Economy Parking","13:00","PM","4/11/2018","15:00","PM","4/11/2018","$ 8.00"},
+                            { "Long-Term Surface Parking","08:00","AM","4/11/2018","21:00","PM","5/11/2018","$ 272.00"},
+                            { "Long-Term Garage Parking","10:00","AM","4/11/2018","16:00","PM","6/11/2018","$ 648.00"},
                     };
         }
     }
